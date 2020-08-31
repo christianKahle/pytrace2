d42158e8a0ae13cd3f5b9e5ebf335c7820d2ef69
christianKahle <ckahle05@gmail.com>
2020-08-31 19:32:40 -0400

    commented code and fixed some bugs

--- entities.py
+++ entities.py
@@ -1,3 +1,6 @@
+# This file contains all constructors for 3D objects, called entities
+# Any entity that will show up on screen should use v_entity or another visible entity as it's subclass
+# Entities should be named by their mathematical names if possible
 from pygame.math import Vector3
 import math
 class entity():
@@ -8,15 +11,25 @@ class entity():
         self.rot.rotate_ip(degrees,rotation_axis)
     def move(self,rel):
         self.pos = self.pos + rel
+
 class v_entity(entity):
     def __init__(self,position,direction,color=(0,0,0)):
         super().__init__(position,direction)
         self.color = Vector3(color)
         self.radius = 0
-    def raysect(self,ray):
+    # Raysect should be overriden and return the distance to the entity (self) along a ray
+    # Otherwise, raysect returns False
+    def raysect(self,ray): 
         return False
+    # ray_normal should be overriden and return the normal direction to the entity at a point
+    # the point is specified by a ray and a distance, where the point is d units along the ray (ray.rot * d + ray.pos)
+    def ray_normal(self,ray,d):
+        return Vector3(0,0,0)
+    # this function should not be overriden
     def global_light_color(self,ray,distance,source):
         return [int(i/2+.5) for i in (Vector3(self.color)*max((-self.ray_normal(ray,distance)*source+1),0.25))]
+# This class can be used to superclass and 2D shapes for use in 3D space
+# ray_normal need not be overriden for any subclass og this class
 class plane(v_entity):
     def __init__(self,position,direction,color=(0,0,0)):
         super().__init__(position,direction,color)
@@ -60,7 +73,7 @@ class sphere(v_entity):
     def ray_normal(self,ray,d):
         y = ray.pos + ray.rot*d
         return (y - self.pos).normalize()
-class rectangle_prism(v_entity):
+class rectangular_prism(v_entity):
     def __init__(self,position,direction,size,up=(),color=(0,0,0)):
         super().__init__(position,direction,color)
         self.size = Vector3(size)/2
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import pygame, math, time
 from entities import *
+# Pixel size of the display surface
 size = (240,135)
 screen = pygame.display.set_mode(size,pygame.SCALED)
 pygame.mouse.set_visible(False)
 
+# Dictionary contain information about player settings
+# TODO implement a settings menu
 playerSettings = {
     "up"          : pygame.math.Vector3(0,0,1),
     "fwd"         : pygame.math.Vector3(1,0,0),
-    "look_angles" : (0,0),
-    "looking"     : pygame.math.Vector3(1,0,0),
+    "look_angles" : (0,-45),
+    "looking"     : pygame.math.Vector3(1,0,-1).normalize(),
     "fov"         : math.radians(90),
     "sensitivity" : 0.75,
     "scroll_sense": math.radians(10),
@@ -18,22 +21,24 @@ playerSettings = {
     "frustum_calc": (16,9)
     }
 
+# Initialize font for use later
 pygame.font.init()
 global_light_dir = -pygame.Vector3(1,1,1).normalize()
 font = pygame.font.SysFont("couriernew", 14)
+# Set mouse to center and reset events
 pygame.mouse.set_pos(size[0]//2,size[1]//2)
 pygame.event.get()
 
-#MAKE SURE TO UNCOMMENT THESE ENTITIES FOR THEM TO SHOW UP
-#inf_plane IS A PLANE WITHOUT LIMITS, IN THIS CASE, THE GROUND
-
+# List of entities to render
 ents = [ 
 #plane((0,0,-2),(0,0,1),color=(0,0,0)),
 #rectangle((1,0,0),(1,0,0),(3,1),color=(200,20,20)),
 #rectangle((0.85,-0.15,0.1),(1,1,1),(1,0.5),up=(1,0,1),color=(20,20,200)),
-sphere((2,0,0.5),(1,0,0),0.35,color=(230,230,30))
-#rectangle_prism((2,0,0),(1,1,0),(1,1,1),(0,0,1),(30,230,130))
+sphere((1,0,-1),(1,0,0),0.65,color=(230,230,30)),
+rectangular_prism((1,0,-1),(1,1,0),(1,1,1),(0,0,1),(30,230,130)),
 ]
+
+# Using the angles found in playerSettings, rotate look vector when the mouse moves
 def rotation(playerInfo,event):
     look_angles = playerInfo["look_angles"]
     up = playerInfo["up"]
@@ -51,6 +56,7 @@ def rotation(playerInfo,event):
     if pygame.display.get_active():
         pygame.mouse.set_pos(size[0]//2,size[1]//2)
 
+# Change the fov found in playerSettings when the mousewheel is scrolled
 def zoom(playerInfo,event):
     b = event.button == 5
     fov = playerInfo["fov"]
@@ -58,6 +64,7 @@ def zoom(playerInfo,event):
     if math.radians(30) < zoom <= math.radians(150):
         playerInfo["fov"] = zoom
 
+# movement lambda functions in a pythonic switch dictionary
 movement = {
     pygame.K_w:lambda pos,fwd,up,speed: pos + fwd * speed,
     pygame.K_s:lambda pos,fwd,up,speed: pos - fwd * speed,
@@ -66,6 +73,7 @@ movement = {
     pygame.K_SPACE:lambda pos,fwd,up,speed: pos + up * speed,
     pygame.K_LSHIFT:lambda pos,fwd,up,speed: pos - up * speed
     }
+# Change the player's position found in playerSettings when a movement key is held
 def move(playerInfo):
     pos, fwd, up, speed = playerInfo["position"], playerInfo["looking"], playerInfo["up"], playerInfo["speed"]
     pressed = pygame.key.get_pressed()
@@ -74,21 +82,25 @@ def move(playerInfo):
             pos = movement.get(i,lambda pos,fwd,up,speed: pos)(pos,fwd,up,speed)
     playerInfo["position"] = pos
 
+# Process pygame events such as key presses and mouse movement
 def processEvents(events,playerInfo):
     close_action = False
     for event in events:
         if event.type == pygame.QUIT:
             close_action = True
-        if event.type == pygame.KEYDOWN:
+        elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DELETE or event.key == pygame.K_BACKSPACE:
                 close_action = True
-        if event.type == pygame.MOUSEMOTION:
+            elif event.key == pygame.K_RETURN:
+                print(playerInfo["position"],playerInfo["look_angles"])
+        elif event.type == pygame.MOUSEMOTION:
             rotation(playerInfo,event)
-        if event.type == pygame.MOUSEBUTTONDOWN:
+        elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button in [4,5]:
                 zoom(playerInfo,event)
     return not close_action
 
+# raycast a single ray, displaying the closest enitity's global light color
 def ray(entities,direction,playerInfo,x,y):
     r = entity(playerInfo["position"],direction)
     short_len = False
@@ -104,16 +116,22 @@ def ray(entities,direction,playerInfo,x,y):
         pygame.draw.rect(screen,shortest.global_light_color(r,short_len,global_light_dir),(x,y,1,1))
     return short_len
 
+# exclude entities from being rendered that are further than the render_dist found in playerSettings
 def max_distance(entities,playerInfo):
-    return [entity for entity in entities if (entity.pos-playerInfo["position"]).length()-entity.radius < playerInfo["render_dist"]]
+    return [entity for entity in entities if (entity.pos-playerInfo["position"]).length() < playerInfo["render_dist"]]
 
+# exclude entities based on left, right, top, and bottom planes
 def frustum(entities,topleft,botright,up,pos):
+    z = topleft * up - botright * up
     left = -topleft.cross(up).normalize()
     right = botright.cross(up).normalize()
-    for n in left,right:
+    top = left.cross(topleft).normalize()
+    bottom = right.cross(botright).normalize()
+    for n in left,right,top,bottom:
         entities = [e for e in entities if (e.pos - pos) * n + e.radius > 0]
     return entities
 
+# update the display surface
 def update(playerInfo,entity_list):
     screen.fill((30,130,230))
     entity_list = max_distance(entity_list,playerInfo)
@@ -134,15 +152,15 @@ def update(playerInfo,entity_list):
                     x_n = x_delta*(2*(x+x2)-size[0])
                     look = (looking + x_n + y_n).normalize()
                     ray(ents,look,playerInfo,x+x2,y+y2)
+    pygame.display.flip()
 
     #pygame.draw.rect(screen,(120,120,120),(size[0]//2,size[1]//2,1,1))
         
-
+# Begin render loop
 current_info = None
 while(processEvents(pygame.event.get(),playerSettings)):
     move(playerSettings)
     if current_info != playerSettings:
         update(playerSettings,ents)
-    pygame.display.flip()
     current_info = playerSettings.copy()
 pygame.quit()
